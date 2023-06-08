5d8a9ef5662355f978f455fcdd3f1e3f7468aa79
HyeonJun0530 <nexus2697@pukyong.ac.kr>
2023-06-08 22:49:31 +0900

    11762 해결

---
+++ src/baekjoon/b11762.java
@@ -0,0 +1,27 @@
+package baekjoon;
+
+import java.io.*;
+import java.util.*;
+
+class b11762 {
+
+    static int[] dp;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken(" "));
+
+        dp = new int[n+1];
+
+        dp[1] = 1;
+        if(n > 1) dp[2] = 2;
+
+        for (int i = 3; i <= n; i++) {
+            dp[i] = (dp[i - 1] + dp[i - 2]) % 10007;
+        }
+
+        System.out.println(dp[n]);
+    }
+}
+
